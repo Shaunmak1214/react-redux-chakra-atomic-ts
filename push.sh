f5acd0b89a85a4ab8f75e3fd9fb73b7e9180fc22
Shaun Mak <60981304+Shaunmak1214@users.noreply.github.com>
2022-01-25 03:34:41 +0800

    feat: initialized 🔥

---
+++ push.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+cmt="Blank"
+
+lf=$'\n'
+    while getopts c:p: opt 2>/dev/null
+    do
+        case $opt in
+            c)
+                cmt=$OPTARG
+                git add .
+                git commit -m "$cmt"
+
+                echo ===================================================
+                echo Pushing to github branch $branch ...
+                echo ===================================================
+                git push origin ${branch}
+                ;;
+            p)
+                base="$OPTARG"
+                echo $cmt
+                echo "Creating pull request to base: $base" >&2
+
+                # Local .env
+                if [ -f .env ]; then
+                    export $(cat .env | grep -v '#' | awk '/=/ {print $1}')
+
+                    curl --location --request POST 'https://api.github.com/repos/Shaunmak1214/TCD2022-Display-Website/pulls' \
+                    --header 'Accept: application/vnd.github.v3+json' \
+                    --header 'Authorization: Basic "'"${BASE_64_GITHUB_PERSONAL_TOKEN}"'"' \
+                    --header 'Content-Type: application/json' \
+                    --data-raw '{
+                      "title": "'"${cmt}"'",
+                      "head": "'"${branch}"'",
+                      "base": "'"${base}"'",
+                      "body": "### Please provide a description of the changes made to the pull request."
+                    }'
+                fi
+
+                ;;
+            * )
+                emsg="${lf}Invalid option '$opt'"
+        esac
+    done
