202d9912a70ea9973d1832da1d7952ddbb512358
Shaun Mak <60981304+Shaunmak1214@users.noreply.github.com>
2022-01-26 05:53:29 +0800

    fix: push.sh small changes

--- push.sh
+++ push.sh
@@ -25,7 +25,7 @@ lf=$'\n'
                 if [ -f .env ]; then
                     export $(cat .env | grep -v '#' | awk '/=/ {print $1}')
 
-                    curl --location --request POST 'https://api.github.com/repos/Shaunmak1214/"'"${REPO_NAME}"'"/pulls' \
+                    curl --location --request POST 'https://api.github.com/repos/Shaunmak1214/'${REPO_NAME}'/pulls' \
                     --header 'Accept: application/vnd.github.v3+json' \
                     --header 'Authorization: Basic "'"${BASE_64_GITHUB_PERSONAL_TOKEN}"'"' \
                     --header 'Content-Type: application/json' \
